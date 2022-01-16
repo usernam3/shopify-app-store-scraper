ca8b87c197429e92a1461fa2a8b0b60ae9badc0b
Stanislav Dobrovolschii <uusername@protonmail.ch>
2022-01-16 17:18:26 +0100

    Fix #3

--- shopify_app_store/spiders/app_store.py
+++ shopify_app_store/spiders/app_store.py
@@ -47,33 +47,36 @@ class AppStoreSpider(scrapy.spiders.SitemapSpider):
         app_id = response.meta['app_id']
 
         url = response.request.url
-        title = response.css('.ui-app-store-hero__header__app-name ::text').extract_first()
-        developer = response.css('.ui-app-store-hero__header__subscript a::text').extract_first()
-        developer_link = response.css('.ui-app-store-hero__header__subscript a::attr(href)').extract_first()
-        icon = response.css('.ui-app-store-hero__app-icon img::attr(src)').extract_first()
-        rating = response.css('.ui-star-rating__rating::text').extract_first()
-        reviews_count = response.css('.ui-review-count-summary a::text').extract_first()
-        description_raw = response.css('.ui-expandable-content .block').extract_first()
-        description = ' '.join(response.css('.ui-expandable-content .block ::text').extract()).strip()
-        tagline = ' '.join(response.css('.ui-app-store-hero__description ::text').extract()).strip()
-        pricing_hint = (response.css('.app-listing-title__sub-heading ::text').extract_first() or '').strip()
+        title = response.css('.vc-app-listing-hero__heading ::text').extract_first(default='').strip()
+        developer = response.css('.vc-app-listing-hero__by-line a::text').extract_first()
+        developer_link = response.css('.vc-app-listing-hero__by-line a::attr(href)').extract_first()
+        icon = response.css('.vc-app-listing-about-section__title img::attr(src)').extract_first()
+        rating = response.css('.ui-star-rating__rating ::text').extract_first()
+        reviews_count = response.css('.ui-review-count-summary a::text').extract_first(default='0 reviews')
+        description_raw = response.css('.vc-app-listing-about-section__description').extract_first()
+        description = ' '.join(response.css('.vc-app-listing-about-section__description ::text').extract()).strip()
+        tagline = ' '.join(response.css('.vc-app-listing-hero__tagline ::text').extract()).strip()
+        pricing_hint = response.css('.ui-app-pricing--format-detail ::text').extract_first(default='').strip()
 
-        for benefit in response.css('.key-benefit'):
-            yield KeyBenefit(app_id=app_id, title=benefit.css('.block__heading ::text').extract_first().strip(),
-                             description=benefit.css('.block__content ::text').extract_first().strip())
+        for benefit in response.css('.vc-app-listing-key-values__item'):
+            yield KeyBenefit(app_id=app_id,
+                             title=benefit.css('.vc-app-listing-key-values__item-title ::text').extract_first().strip(),
+                             description=benefit.css(
+                                 '.vc-app-listing-key-values__item-description ::text').extract_first().strip())
 
         for pricing_plan in response.css('.ui-card.pricing-plan-card'):
             pricing_plan_id = str(uuid.uuid4())
             yield PricingPlan(id=pricing_plan_id,
                               app_id=app_id,
-                              title=(pricing_plan.css('h5 ::text').extract_first() or '').strip(),
+                              title=pricing_plan.css('.pricing-plan-card__title-kicker ::text').extract_first(
+                                  default='').strip(),
                               price=pricing_plan.css('h3 ::text').extract_first().strip())
 
-            for feature in pricing_plan.css('ul li .bullet__content'):
+            for feature in pricing_plan.css('ul li.bullet'):
                 yield PricingPlanFeature(pricing_plan_id=pricing_plan_id, app_id=app_id,
-                                         feature=feature.css('::text').extract_first().strip())
+                                         feature=' '.join(feature.css('::text').extract()).strip())
 
-        for category in response.css('.ui-app-store-hero__kicker a::text').extract():
+        for category in response.css('.vc-app-listing-hero__taxonomy-links a::text').extract():
             category_id = hashlib.md5(category.lower().encode()).hexdigest()
 
             yield Category(id=category_id, title=category)
